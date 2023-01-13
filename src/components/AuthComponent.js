b8f6675f40eeaf45ae8977234f4816af61d9050c
unknown <gm.c.burzo@gmail.com>
2023-01-12 21:24:46 -0500

    i

--- src/components/AuthComponent.js
+++ src/components/AuthComponent.js
@@ -1,11 +1,11 @@
 import { useRownd } from '@rownd/react';
+import { Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function SignInOrLogIn() {
-
   const { user, is_authenticated, requestSignIn } = useRownd(); 
-
+  
   useEffect(() => {
     if (!is_authenticated) {
       requestSignIn();
@@ -16,6 +16,8 @@ function SignInOrLogIn() {
     <div>
       <h3>Please reload this page or Sign In using the bottom left tab</h3>
       <Link to="/">Or Click here to go back to home page</Link>
+      <p>----------------------------------------------------------------</p>
+      <Navigate replace to="/usertest"/>
     </div>
   );
 }
--- src/components/userTest.js
+++ src/components/userTest.js
@@ -3,17 +3,40 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function UserTest() {
-//const { user, requestSignIn } = useRownd(); 
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [clicked, setclicked] = useState(false);
+  const { user, requestSignIn } = useRownd(); 
 
-  console.log("USER::", rownd.user);
+  //console.log("USER::", window.rownd.user);
 
-  rownd.user.setValue('first_name', 'Alice');
+  const numberHandler = (e) =>{
+    e.preventDefault();
+    setPhoneNumber(e.target.value);
+  }
+
+  const nameHandler = (e) =>{
+    e.preventDefault();
+    setFirstName(e.target.value);
+  }
+
+  const clickHandler = () =>{
+    if(firstName != null){
+      setclicked(true);
+    }
+  }
+
+  useEffect(()=>{
+    if (clicked == true) {
+      window.rownd.user.setValue('first_name', firstName);
+    }
+  }, []);
   
-
   return (
     <div>
-      <h3>ERROR HERE</h3>
       <Link to="/">Or Click here to go back to home page</Link>
+      <input onChange = {nameHandler} value={firstName}/>
+      <button onClick={clickHandler}>Click To finish with Email</button>
     </div>
   );
 }
